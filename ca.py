3f803c3c873e28bab75fb5b1d3776548850e112a
Mathieu RENARD <dark@gotohack.org>
2016-03-18 13:32:40 +0100

    Fixed PKCS#1 to PKCS#8 hack

--- ca.py
+++ ca.py
@@ -1,44 +1,52 @@
 #!/usr/bin/env python
-  
+
 """
 How to create a CA certificate with Python.
-  
+
 WARNING: This sample only demonstrates how to use the objects and methods,
          not how to create a safe and correct certificate.
-  
+
 Copyright (c) 2004 Open Source Applications Foundation.
 Author: Heikki Toivonen
 """
-  
+
 from M2Crypto import RSA, X509, EVP, m2, Rand, Err, BIO
 from M2Crypto.RSA import load_pub_key_bio
 import base64
-  
+
+
+def der_length(length):
+    #DER encoding of a length
+    if length < 128:
+        return chr(length)
+    prefix = 0x80
+    result = ''
+    while length > 0:
+        result = chr(length & 0xff) + result
+        length >>= 8
+        prefix += 1
+    return chr(prefix) + result
+
 def convertPKCS1toPKCS8pubKey(data):
-    subjectpublickeyrsa_start = "30 81 9E 30 0D 06 09 2A 86 48 86 F7 0D 01 01 01 05 00 03 81 8C 00".replace(" ","").decode("hex")
-    data = data.replace("-----BEGIN RSA PUBLIC KEY-----\n","").replace("-----END RSA PUBLIC KEY-----","")
-    data = base64.b64decode(data)
-    if data.startswith("30 81 89 02 81 81 00".replace(" ","").decode("hex")):
-        #HAX remove null byte to make 128 bytes modulus
-        data = "30 81 88 02 81 80".replace(" ","").decode("hex") + data[7:]
-    z = base64.b64encode(subjectpublickeyrsa_start + data)
-    res = "-----BEGIN PUBLIC KEY-----\n"
-    for i in xrange(len(z)/64 + 1):
-        res += z[i*64:(i+1)*64] + "\n"
-    res += "-----END PUBLIC KEY-----"
+    #https://mail.python.org/pipermail/python-list/2013-January/638754.html
+    res = data.split('\n')
+    res = '\0' + base64.decodestring("".join(res[1:-2]))
+    res = '\x30\x0d\x06\x09\x2a\x86\x48\x86\xf7\x0d\x01\x01\x01\x05\x00\x03' + der_length(len(res)) + res
+    res = '\x30' + der_length(len(res)) + res
+    res = '-----BEGIN PUBLIC KEY-----\n' + base64.encodestring(res) + '-----END PUBLIC KEY-----'
     return res
 
 # XXX Do I actually need more keys?
 # XXX Check return values from functions
-  
+
 def generateRSAKey():
     return RSA.gen_key(2048, m2.RSA_F4)
-  
+
 def makePKey(key):
     pkey = EVP.PKey()
     pkey.assign_rsa(key)
     return pkey
-  
+
 def makeRequest(pkey, cn):
     req = X509.Request()
     req.set_version(2)
@@ -51,13 +59,13 @@ def makeRequest(pkey, cn):
     extstack = X509.X509_Extension_Stack()
     extstack.push(ext1)
     extstack.push(ext2)
-  
+
     assert(extstack[1].get_name() == 'nsComment')
-  
+
     req.add_extensions(extstack)
     #req.sign(pkey, 'sha1')
     return req
-  
+
 def makeCert(req, caPkey):
     pkey = req.get_pubkey()
     #woop = makePKey(generateRSAKey())
@@ -92,34 +100,33 @@ def makeCert(req, caPkey):
     ext.set_critical(0)# Defaults to non-critical, but we can also set it
     cert.add_ext(ext)
     cert.sign(caPkey, 'sha1')
-  
+
     assert(cert.get_ext('subjectAltName').get_name() == 'subjectAltName')
     assert(cert.get_ext_at(0).get_name() == 'subjectAltName')
     assert(cert.get_ext_at(0).get_value() == 'DNS:foobar.example.com')
-  
+
     return cert
-  
+
 def ca():
     key = generateRSAKey()
     pkey = makePKey(key)
     req = makeRequest(pkey)
     cert = makeCert(req, pkey)
     return (cert, pkey)
-  
+
 
 def ca_do_everything(DevicePublicKey):
     rsa = generateRSAKey()
     privateKey = makePKey(rsa)
     req = makeRequest(privateKey, "The Issuer Monkey")
     cert = makeCert(req, privateKey)
-    
     rsa2 = load_pub_key_bio(BIO.MemoryBuffer(convertPKCS1toPKCS8pubKey(DevicePublicKey)))
     pkey2 = EVP.PKey()
     pkey2.assign_rsa(rsa2)
     req = makeRequest(pkey2, "Device")
     cert2 = makeCert(req, privateKey)
     return cert.as_pem(), privateKey.as_pem(None), cert2.as_pem()
-    
+
 if __name__ == '__main__':
     rsa = generateRSAKey()
     pkey = makePKey(rsa)
