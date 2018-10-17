f0cf5f0b4b03a406baa1fdb714273b5df122e6b8
JerryGinger <husteris@gmail.com>
2018-10-17 17:06:09 +0800

    add travis ci file

---
+++ test/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+import unittest
+import os
+import sys
+
+test_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.dirname(test_dir))
+
+def main():
+    runner = unittest.TextTestRunner(verbosity=1 + sys.argv.count('-v'))
+    suite = unittest.TestLoader().discover(test_dir,  pattern='*test.py')
+    raise SystemExit(not runner.run(suite).wasSuccessful())
+
+
+if __name__ == '__main__':
+    main()
