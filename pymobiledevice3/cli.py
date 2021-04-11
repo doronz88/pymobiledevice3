b7459070fa06c669a334119029dac0924bd91154
doron zarhi <doron88@gmail.com>
2021-04-11 19:09:58 +0300

    cli: refactor udid passing to all commands

--- pymobiledevice3/cli.py
+++ pymobiledevice3/cli.py
@@ -39,6 +39,18 @@ def print_object(buf, colored=True):
         pprint(buf)
 
 
+class Command(click.Command):
+    @staticmethod
+    def udid(ctx, param, value):
+        return LockdownClient(udid=value)
+
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+        self.params[:0] = [
+            click.Option(('lockdown', '--udid'), callback=self.udid)
+        ]
+
+
 @click.group()
 def cli():
     pass
@@ -50,13 +62,11 @@ def apps():
     pass
 
 
-@apps.command('list')
-@click.option('--udid')
+@apps.command('list', cls=Command)
 @click.option('-u', '--user', is_flag=True, help='include user apps')
 @click.option('-s', '--system', is_flag=True, help='include system apps')
-def apps_list(udid, user, system):
+def apps_list(lockdown, user, system):
     """ list installed apps """
-    lockdown = LockdownClient(udid=udid)
     app_types = []
     if user:
         app_types.append('User')
@@ -65,21 +75,17 @@ def apps_list(udid, user, system):
     pprint(InstallationProxyService(lockdown=lockdown).get_apps(app_types))
 
 
-@apps.command('uninstall')
-@click.option('--udid')
+@apps.command('uninstall', cls=Command)
 @click.argument('bundle_id')
-def apps_uninstall(udid, bundle_id):
+def apps_uninstall(lockdown, bundle_id):
     """ uninstall app by given bundle_id """
-    lockdown = LockdownClient(udid=udid)
     pprint(InstallationProxyService(lockdown=lockdown).uninstall(bundle_id))
 
 
-@apps.command('install')
-@click.option('--udid')
+@apps.command('install', cls=Command)
 @click.argument('ipa_path', type=click.Path(exists=True))
-def apps_install(udid, ipa_path):
+def apps_install(lockdown, ipa_path):
     """ install given .ipa """
-    lockdown = LockdownClient(udid=udid)
     pprint(InstallationProxyService(lockdown=lockdown).install_from_local(ipa_path))
 
 
@@ -89,29 +95,23 @@ def config():
     pass
 
 
-@config.command('list')
-@click.option('--udid')
-def config_list(udid):
+@config.command('list', cls=Command)
+def config_list(lockdown):
     """ list installed profiles """
-    lockdown = LockdownClient(udid=udid)
     pprint(MobileConfigService(lockdown=lockdown).get_profile_list())
 
 
-@config.command('install')
-@click.option('--udid')
+@config.command('install', cls=Command)
 @click.argument('profile', type=click.File('rb'))
-def config_install(udid, profile):
+def config_install(lockdown, profile):
     """ install given profile file """
-    lockdown = LockdownClient(udid=udid)
     pprint(MobileConfigService(lockdown=lockdown).install_profile(profile.read()))
 
 
-@config.command('remove')
-@click.option('--udid')
+@config.command('remove', cls=Command)
 @click.argument('name')
-def config_remove(udid, name):
+def config_remove(lockdown, name):
     """ remove profile by name """
-    lockdown = LockdownClient(udid=udid)
     pprint(MobileConfigService(lockdown=lockdown).remove_profile(name))
 
 
@@ -121,19 +121,16 @@ def lockdown():
     pass
 
 
-@lockdown.command('recovery')
-@click.option('--udid')
-def lockdown_recovery(udid):
+@lockdown.command('recovery', cls=Command)
+def lockdown_recovery(lockdown):
     """ enter recovery """
-    pprint(LockdownClient(udid=udid).enter_recovery())
+    pprint(lockdown.enter_recovery())
 
 
-@lockdown.command('service')
-@click.option('--udid')
+@lockdown.command('service', cls=Command)
 @click.argument('service_name')
-def lockdown_service(udid, service_name):
+def lockdown_service(lockdown, service_name):
     """ send-receive raw service messages """
-    lockdown = LockdownClient(udid=udid)
     client = lockdown.start_service(service_name)
     logging.info('use `client` variable to interact with the connected service')
     IPython.embed()
@@ -145,55 +142,43 @@ def diagnostics():
     pass
 
 
-@diagnostics.command('restart')
-@click.option('--udid')
-def diagnostics_restart(udid):
+@diagnostics.command('restart', cls=Command)
+def diagnostics_restart(lockdown):
     """ restart device """
-    lockdown = LockdownClient(udid=udid)
     DiagnosticsService(lockdown=lockdown).restart()
 
 
-@diagnostics.command('shutdown')
-@click.option('--udid')
-def diagnostics_shutdown(udid):
+@diagnostics.command('shutdown', cls=Command)
+def diagnostics_shutdown(lockdown):
     """ shutdown device """
-    lockdown = LockdownClient(udid=udid)
     DiagnosticsService(lockdown=lockdown).shutdown()
 
 
-@diagnostics.command('sleep')
-@click.option('--udid')
-def diagnostics_sleep(udid):
+@diagnostics.command('sleep', cls=Command)
+def diagnostics_sleep(lockdown):
     """ put device into sleep """
-    lockdown = LockdownClient(udid=udid)
     DiagnosticsService(lockdown=lockdown).sleep()
 
 
-@diagnostics.command('info')
-@click.option('--udid')
-def diagnostics_info(udid):
+@diagnostics.command('info', cls=Command)
+def diagnostics_info(lockdown):
     """ get diagnostics info """
-    lockdown = LockdownClient(udid=udid)
     pprint(DiagnosticsService(lockdown=lockdown).info())
 
 
-@diagnostics.command('ioregistry')
-@click.option('--udid')
+@diagnostics.command('ioregistry', cls=Command)
 @click.option('--plane')
 @click.option('--name')
 @click.option('--ioclass')
-def diagnostics_ioregistry(udid, plane, name, ioclass):
+def diagnostics_ioregistry(lockdown, plane, name, ioclass):
     """ get ioregistry info """
-    lockdown = LockdownClient(udid=udid)
     pprint(DiagnosticsService(lockdown=lockdown).ioregistry(plane=plane, name=name, ioclass=ioclass))
 
 
-@diagnostics.command('mg')
-@click.option('--udid')
+@diagnostics.command('mg', cls=Command)
 @click.argument('keys', nargs=-1, default=None)
-def diagnostics_mg(udid, keys):
+def diagnostics_mg(lockdown, keys):
     """ get MobileGestalt key values from given list. If empty, return all known. """
-    lockdown = LockdownClient(udid=udid)
     pprint(DiagnosticsService(lockdown=lockdown).mobilegestalt(keys=keys))
 
 
@@ -203,15 +188,13 @@ def syslog():
     pass
 
 
-@syslog.command('live')
-@click.option('--udid')
+@syslog.command('live', cls=Command)
 @click.option('-o', '--out', type=click.File('wt'), help='log file')
 @click.option('--nocolor', is_flag=True, help='disable colors')
 @click.option('--pid', type=click.INT, default=-1, help='pid to filter. -1 for all')
 @click.option('-m', '--match', help='match expression')
-def syslog_live(udid, out, nocolor, pid, match):
+def syslog_live(lockdown, out, nocolor, pid, match):
     """ view live syslog lines """
-    lockdown = LockdownClient(udid=udid)
     for syslog_entry in OsTraceService(lockdown=lockdown).syslog(pid=pid):
         pid = syslog_entry.pid
         timestamp = syslog_entry.timestamp
@@ -250,45 +233,37 @@ def syslog_live(udid, out, nocolor, pid, match):
             out.write(line)
 
 
-@syslog.command('archive')
-@click.option('--udid')
+@syslog.command('archive', cls=Command)
 @click.argument('out', type=click.File('wb'))
-def syslog_archive(udid, out):
+def syslog_archive(lockdown, out):
     """
     create PAX archive.
     use `pax -r < filename` for extraction.
     """
-    lockdown = LockdownClient(udid=udid)
     result, tar = OsTraceService(lockdown=lockdown).create_archive()
     out.write(tar)
 
 
-@cli.command()
-@click.option('--udid')
+@cli.command(cls=Command)
 @click.argument('out', type=click.File('wb'), required=False)
-def pcap(udid, out):
+def pcap(lockdown, out):
     """ sniff device traffic """
-    lockdown = LockdownClient(udid=udid)
     PcapdService(lockdown=lockdown).watch(out=out)
 
 
-@cli.command()
-@click.option('--udid')
+@cli.command(cls=Command)
 @click.argument('out', type=click.File('wb'))
-def screenshot(udid, out):
+def screenshot(lockdown, out):
     """ take a screenshot in TIFF format """
-    lockdown = LockdownClient(udid=udid)
     out.write(ScreenshotService(lockdown=lockdown).take_screenshot())
 
 
-@cli.command()
-@click.option('--udid')
+@cli.command(cls=Command)
 @click.argument('action', type=click.Choice(['flush', 'shell']))
-def crash(udid, action):
+def crash(lockdown, action):
     """ crash utils """
     if action == 'flush':
         ack = b'ping\x00'
-        lockdown = LockdownClient(udid=udid)
         assert ack == lockdown.start_service('com.apple.crashreportmover').recv_exact(len(ack))
     elif action == 'shell':
         AFCShell(udid=udid, afcname='com.apple.crashreportcopymobile').cmdloop()
@@ -300,66 +275,53 @@ def afc():
     pass
 
 
-@afc.command('shell')
-@click.option('--udid')
-def afc_shell(udid):
+@afc.command('shell', cls=Command)
+def afc_shell(lockdown):
     """ open an AFC shell rooted at /var/mobile/Media """
-    AFCShell(udid=udid, afcname='com.apple.afc').cmdloop()
+    AFCShell(lockdown=lockdown, afcname='com.apple.afc').cmdloop()
 
 
-@afc.command('pull')
-@click.option('--udid')
+@afc.command('pull', cls=Command)
 @click.argument('remote_file', type=click.Path(exists=False))
 @click.argument('local_file', type=click.File('wb'))
-def afc_pull(udid, remote_file, local_file):
+def afc_pull(lockdown, remote_file, local_file):
     """ open an AFC shell rooted at /var/mobile/Media """
-    lockdown = LockdownClient(udid=udid)
     local_file.write(AFCClient(lockdown=lockdown).get_file_contents(remote_file))
 
 
-@afc.command('push')
-@click.option('--udid')
+@afc.command('push', cls=Command)
 @click.argument('local_file', type=click.File('rb'))
 @click.argument('remote_file', type=click.Path(exists=False))
-def afc_push(udid, local_file, remote_file):
+def afc_push(lockdown, local_file, remote_file):
     """ open an AFC shell rooted at /var/mobile/Media """
-    lockdown = LockdownClient(udid=udid)
     AFCClient(lockdown=lockdown).set_file_contents(remote_file, local_file.read())
 
 
-@afc.command('ls')
-@click.option('--udid')
+@afc.command('ls', cls=Command)
 @click.argument('remote_file', type=click.Path(exists=False))
-def afc_ls(udid, remote_file):
+def afc_ls(lockdown, remote_file):
     """ open an AFC shell rooted at /var/mobile/Media """
-    lockdown = LockdownClient(udid=udid)
     pprint(AFCClient(lockdown=lockdown).read_directory(remote_file))
 
 
-@afc.command('rm')
-@click.option('--udid')
+@afc.command('rm', cls=Command)
 @click.argument('remote_file', type=click.Path(exists=False))
-def afc_rm(udid, remote_file):
+def afc_rm(lockdown, remote_file):
     """ open an AFC shell rooted at /var/mobile/Media """
-    lockdown = LockdownClient(udid=udid)
     AFCClient(lockdown=lockdown).file_remove(remote_file)
 
 
-@cli.command()
-@click.option('--udid')
-def ps(udid):
+@cli.command(cls=Command)
+def ps(lockdown):
     """ show process list """
-    lockdown = LockdownClient(udid=udid)
     pprint(OsTraceService(lockdown=lockdown).get_pid_list())
 
 
-@cli.command()
-@click.option('--udid')
+@cli.command(cls=Command)
 @click.argument('action', type=click.Choice(['post', 'observe']))
 @click.argument('names', nargs=-1)
-def notification(udid, action, names):
+def notification(lockdown, action, names):
     """ API for notify_post() & notify_register_dispatch(). """
-    lockdown = LockdownClient(udid=udid)
     service = NotificationProxyService(lockdown=lockdown)
     for name in names:
         if action == 'post':
@@ -378,12 +340,10 @@ def developer():
     pass
 
 
-@developer.command('proclist')
-@click.option('--udid')
+@developer.command('proclist', cls=Command)
 @click.option('--nocolor', is_flag=True)
-def proclist(udid, nocolor):
+def proclist(lockdown, nocolor):
     """ show process list """
-    lockdown = LockdownClient(udid=udid)
     with DvtSecureSocketProxyService(lockdown=lockdown) as dvt:
         processes = dvt.proclist()
         for process in processes:
@@ -393,43 +353,36 @@ def proclist(udid, nocolor):
         print_object(processes, colored=not nocolor)
 
 
-@developer.command('applist')
-@click.option('--udid')
+@developer.command('applist', cls=Command)
 @click.option('--nocolor', is_flag=True)
-def applist(udid, nocolor):
+def applist(lockdown, nocolor):
     """ show application list """
-    lockdown = LockdownClient(udid=udid)
     with DvtSecureSocketProxyService(lockdown=lockdown) as dvt:
         apps = dvt.applist()
         print_object(apps, colored=not nocolor)
 
 
-@developer.command('kill')
-@click.option('--udid')
+@developer.command('kill', cls=Command)
 @click.argument('pid', type=click.INT)
-def kill(udid, pid):
+def kill(lockdown, pid):
     """ Kill a process by its pid. """
-    lockdown = LockdownClient(udid=udid)
     with DvtSecureSocketProxyService(lockdown=lockdown) as dvt:
         dvt.kill(pid)
 
 
-@developer.command('launch')
-@click.option('--udid')
+@developer.command('launch', cls=Command)
 @click.argument('bundle_id', type=click.STRING)
-def launch(udid, bundle_id):
+def launch(lockdown, bundle_id):
     """ Kill a process by its pid. """
-    lockdown = LockdownClient(udid=udid)
     with DvtSecureSocketProxyService(lockdown=lockdown) as dvt:
         pid = dvt.launch(bundle_id)
         print(f'Process launched with pid {pid}')
 
 
-@developer.command('shell')
-@click.option('--udid')
-def shell(udid):
+@developer.command('shell', cls=Command)
+def shell(lockdown):
     """ Launch developer shell. """
-    with DvtSecureSocketProxyService(lockdown=LockdownClient(udid=udid)) as dvt:
+    with DvtSecureSocketProxyService(lockdown=lockdown) as dvt:
         dvt.shell()
 
 
