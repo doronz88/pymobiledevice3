10c63ce3113c501234b85a6103acbdd2539ea63f
doron zarhi <doron88@gmail.com>
2021-04-12 13:21:59 +0300

    cli: add mounter options

--- pymobiledevice3/cli.py
+++ pymobiledevice3/cli.py
@@ -6,7 +6,6 @@ import json
 
 from termcolor import colored
 import coloredlogs
-import IPython
 import click
 from pygments import highlight, lexers, formatters
 
@@ -15,6 +14,7 @@ from pymobiledevice3.diagnostics_service import DiagnosticsService
 from pymobiledevice3.installation_proxy_service import InstallationProxyService
 from pymobiledevice3.lockdown import LockdownClient
 from pymobiledevice3.mobile_config import MobileConfigService
+from pymobiledevice3.mobile_image_mounter_service import MobileImageMounterService
 from pymobiledevice3.notification_proxy_service import NotificationProxyService
 from pymobiledevice3.os_trace_service import OsTraceService
 from pymobiledevice3.pcapd_service import PcapdService
@@ -54,6 +54,66 @@ class Command(click.Command):
 @click.group()
 def cli():
     pass
+
+
+@cli.group()
+def mounter():
+    """ mounter options """
+    pass
+
+
+@mounter.command('list', cls=Command)
+def mounter_list(lockdown):
+    """ lookup mounter image type """
+    pprint(MobileImageMounterService(lockdown=lockdown).list_images())
+
+
+@mounter.command('lookup', cls=Command)
+@click.argument('image_type')
+def mounter_lookup(lockdown, image_type):
+    """ lookup mounter image type """
+    pprint(MobileImageMounterService(lockdown=lockdown).lookup_image(image_type))
+
+
+@mounter.command('umount', cls=Command)
+def mounter_umount(lockdown):
+    """ unmount developer image. """
+    image_type = 'Developer'
+    mount_path = '/Developer'
+    image_mounter = MobileImageMounterService(lockdown=lockdown)
+    image_mounter.umount(image_type, mount_path, b'')
+
+
+@mounter.command('mount', cls=Command)
+@click.option('-i', '--image', type=click.Path(exists=True))
+@click.option('-s', '--signature', type=click.Path(exists=True))
+@click.option('-x', '--xcode', type=click.Path(exists=True, dir_okay=True, file_okay=False),
+              default='/Applications/Xcode.app',
+              help='Xcode application path used to figure out automatically the DeveloperDiskImage path')
+@click.option('-v', '--version', help='use a different DeveloperDiskImage version from the one retrieved by lockdown'
+                                      'connection')
+def mounter_mount(lockdown, image, signature, xcode, version):
+    """ mount developer image. """
+    image_type = 'Developer'
+
+    if image and signature:
+        logging.info('using given image and signature for mount command')
+    else:
+        logging.info('trying to figure out the best suited DeveloperDiskImage')
+        if version is None:
+            version = lockdown.ios_version
+        image = f'{xcode}/Contents/Developer/Platforms/iPhoneOS.platform/DeviceSupport/{version}/DeveloperDiskImage.dmg'
+        signature = f'{image}.signature'
+
+    with open(image, 'rb') as image:
+        image = image.read()
+
+    with open(signature, 'rb') as signature:
+        signature = signature.read()
+
+    image_mounter = MobileImageMounterService(lockdown=lockdown)
+    image_mounter.upload_image(image_type, image, signature)
+    image_mounter.mount(image_type, signature)
 
 
 @cli.group()
@@ -387,14 +447,6 @@ def shell(lockdown):
     """ Launch developer shell. """
     with DvtSecureSocketProxyService(lockdown=lockdown) as dvt:
         dvt.shell()
-
-
-@developer.command('ls', cls=Command)
-@click.argument('path', type=click.Path(exists=False))
-def ls(lockdown, path):
-    """ Launch developer shell. """
-    with DvtSecureSocketProxyService(lockdown=lockdown) as dvt:
-        pprint(dvt.ls(path))
 
 
 @developer.command('ls', cls=Command)
