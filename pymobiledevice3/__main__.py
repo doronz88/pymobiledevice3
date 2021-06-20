c0e959faa1242a63dd3898bcedbbf2fbfff9e257
doron zarhi <doron88@gmail.com>
2021-06-20 14:20:08 +0300

    cli: add support for power assertions

--- pymobiledevice3/__main__.py
+++ pymobiledevice3/__main__.py
@@ -17,6 +17,7 @@ from pymobiledevice3.cli.afc import cli as afc_cli
 from pymobiledevice3.cli.processes import cli as ps_cli
 from pymobiledevice3.cli.notification import cli as notification_cli
 from pymobiledevice3.cli.list_devices import cli as list_devices_cli
+from pymobiledevice3.cli.power_assertion import cli as power_assertion_cli
 
 coloredlogs.install(level=logging.DEBUG)
 
@@ -30,7 +31,7 @@ logging.getLogger('humanfriendly.prompts').disabled = True
 def cli():
     cli_commands = click.CommandCollection(sources=[
         developer_cli, mounter_cli, apps_cli, profile_cli, lockdown_cli, diagnostics_cli, syslog_cli, pcap_cli,
-        crash_cli, afc_cli, ps_cli, notification_cli, list_devices_cli
+        crash_cli, afc_cli, ps_cli, notification_cli, list_devices_cli, power_assertion_cli,
     ])
     cli_commands()
 
---
+++ pymobiledevice3/services/power_assertion.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import logging
+import time
+
+from pymobiledevice3.lockdown import LockdownClient
+
+
+class PowerAssertionService:
+    SERVICE_NAME = 'com.apple.mobile.assertion_agent'
+
+    def __init__(self, lockdown: LockdownClient):
+        self.logger = logging.getLogger(__name__)
+        self.lockdown = lockdown
+        self.service = self.lockdown.start_service(self.SERVICE_NAME)
+
+    def create_power_assertion(self, type_, name, timeout, details=None):
+        msg = {
+            'CommandKey': 'CommandCreateAssertion',
+            'AssertionTypeKey': type_,
+            'AssertionNameKey': name,
+            'AssertionTimeoutKey': timeout,
+        }
+
+        if details is not None:
+            msg['AssertionDetailKey'] = details
+
+        self.service.send_recv_plist(msg)
+        time.sleep(timeout)
