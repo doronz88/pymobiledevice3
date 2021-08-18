2cc26a555ea9f9ae7f30996717f9f49da60005cc
doron zarhi <doron88@gmail.com>
2021-08-18 13:27:07 +0300

    bump version to 1.9.12

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 
 BASE_DIR = os.path.realpath(os.path.dirname(__file__))
-VERSION = '1.9.11'
+VERSION = '1.9.12'
 
 
 def parse_requirements():
