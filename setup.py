36f81d18639c7594a0173893a5a6c16402ddac5b
doron zarhi <doron88@gmail.com>
2021-08-29 15:21:14 +0300

    bump version to 1.10.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 
 BASE_DIR = os.path.realpath(os.path.dirname(__file__))
-VERSION = '1.10.2'
+VERSION = '1.10.3'
 
 
 def parse_requirements():
