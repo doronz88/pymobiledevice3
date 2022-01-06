0ebe20595c09565444917c4a6075a603db5110f8
DoronZ <doron88@gmail.com>
2022-01-06 18:05:08 +0200

    feature: crash_reports: seperate and improve parsing into pycrashreports

--- pymobiledevice3/cli/crash.py
+++ pymobiledevice3/cli/crash.py
@@ -2,8 +2,7 @@ import click
 
 from pymobiledevice3.cli.cli_common import Command
 from pymobiledevice3.lockdown import LockdownClient
-from pymobiledevice3.services.afc import AfcShell
-from pymobiledevice3.services.crash_reports import CrashReports
+from pymobiledevice3.services.crash_reports import CrashReportsManager, CrashReportsShell
 
 
 @click.group()
@@ -21,7 +20,7 @@ def crash():
 @crash.command('clear', cls=Command)
 def crash_clear(lockdown: LockdownClient):
     """ clear(/remove) all crash reports """
-    CrashReports(lockdown).clear()
+    CrashReportsManager(lockdown).clear()
 
 
 @crash.command('pull', cls=Command)
@@ -32,13 +31,13 @@ def crash_pull(lockdown: LockdownClient, out, remote_file, erase):
     """ pull all crash reports """
     if remote_file is None:
         remote_file = '/'
-    CrashReports(lockdown).pull(out, remote_file, erase)
+    CrashReportsManager(lockdown).pull(out, remote_file, erase)
 
 
 @crash.command('shell', cls=Command)
 def crash_shell(lockdown: LockdownClient):
     """ start an afc shell """
-    AfcShell(lockdown=lockdown, service_name=CrashReports.COPY_MOBILE_NAME).cmdloop()
+    CrashReportsShell(lockdown=lockdown).cmdloop()
 
 
 @crash.command('ls', cls=Command)
@@ -48,14 +47,14 @@ def crash_ls(lockdown: LockdownClient, remote_file, depth):
     """ List  """
     if remote_file is None:
         remote_file = '/'
-    for path in CrashReports(lockdown).ls(remote_file, depth):
+    for path in CrashReportsManager(lockdown).ls(remote_file, depth):
         print(path)
 
 
 @crash.command('flush', cls=Command)
 def crash_mover_flush(lockdown: LockdownClient):
     """ trigger com.apple.crashreportmover to flush all products into CrashReports directory """
-    CrashReports(lockdown).flush()
+    CrashReportsManager(lockdown).flush()
 
 
 @crash.command('watch', cls=Command)
@@ -63,7 +62,7 @@ def crash_mover_flush(lockdown: LockdownClient):
 @click.option('-r', '--raw', is_flag=True)
 def crash_mover_watch(lockdown: LockdownClient, name, raw):
     """ watch for crash report generation """
-    for crash_report in CrashReports(lockdown).watch(name=name, raw=raw):
+    for crash_report in CrashReportsManager(lockdown).watch(name=name, raw=raw):
         print(crash_report)
 
 
@@ -73,4 +72,4 @@ def crash_mover_watch(lockdown: LockdownClient, name, raw):
 def crash_sysdiagnose(lockdown: LockdownClient, out, erase):
     """ get a sysdiagnose archive from device (requires user interaction) """
     print('Press Power+VolUp+VolDown for 0.215 seconds')
-    CrashReports(lockdown).get_new_sysdiagnose(out, erase=erase)
+    CrashReportsManager(lockdown).get_new_sysdiagnose(out, erase=erase)
--- pymobiledevice3/services/crash_reports.py
+++ pymobiledevice3/services/crash_reports.py
@@ -1,201 +1,15 @@
-import json
 import logging
 import posixpath
-from collections import namedtuple
-from typing import List, Optional
 
-import click
-from cached_property import cached_property
+from cmd2 import with_argparser, Cmd2ArgumentParser
+from pycrashreport.crash_report import CrashReport
+
 from pymobiledevice3.lockdown import LockdownClient
-from pymobiledevice3.services.afc import AfcService
+from pymobiledevice3.services.afc import AfcService, AfcShell
 from pymobiledevice3.services.os_trace import OsTraceService
 
-Frame = namedtuple('Frame', 'image symbol offset')
-Register = namedtuple('Register', 'name value')
 
-
-class CrashReport:
-    def __init__(self, buf: str):
-        self._metadata, self._data = buf.split('\n', 1)
-        self._metadata = json.loads(self._metadata)
-        self._parse()
-
-    def _parse(self):
-        self._is_json = False
-        try:
-            self._data = json.loads(self._data)
-            self._is_json = True
-        except json.decoder.JSONDecodeError:
-            pass
-
-    def _parse_field(self, name: str) -> str:
-        name += ':'
-        for line in self._data.split('\n'):
-            if line.startswith(name):
-                field = line.split(name, 1)[1]
-                field = field.strip()
-                return field
-
-    @cached_property
-    def bug_type(self):
-        return self._metadata['bug_type']
-
-    @cached_property
-    def incident_id(self):
-        return self._metadata['incident_id']
-
-    @cached_property
-    def timestamp(self):
-        return self._metadata['timestamp']
-
-    @cached_property
-    def name(self) -> str:
-        return self._metadata['name']
-
-    @cached_property
-    def faulting_thread(self) -> int:
-        if self._is_json:
-            return self._data['faultingThread']
-        else:
-            return int(self._parse_field('Triggered by Thread'))
-
-    @cached_property
-    def frames(self) -> List[Frame]:
-        result = []
-        if self._is_json:
-            thread_index = self.faulting_thread
-            images = self._data['usedImages']
-            for frame in self._data['threads'][thread_index]['frames']:
-                image = images[frame['imageIndex']]
-                result.append(
-                    Frame(image=image['path'], symbol=hex(image['base']), offset=frame['imageOffset']))
-        else:
-            in_frames = False
-            for line in self._data.split('\n'):
-                if in_frames:
-                    splitted = line.split()
-
-                    if len(splitted) == 0:
-                        break
-
-                    assert splitted[-2] == '+'
-                    result.append(Frame(image=splitted[1], symbol=splitted[-3], offset=int(splitted[-1])))
-
-                if line.startswith(f'Thread {self.faulting_thread} Crashed:'):
-                    in_frames = True
-
-        return result
-
-    @cached_property
-    def registers(self) -> List[Register]:
-        result = []
-        if self._is_json:
-            thread_index = self._data['faultingThread']
-            thread_state = self._data['threads'][thread_index]['threadState']
-
-            for i, reg in enumerate(thread_state['x']):
-                result.append(Register(name=f'x{i}', value=reg['value']))
-
-            additional_regs = ('lr', 'cpsr', 'fp', 'sp', 'esr', 'pc', 'far')
-
-            for reg in additional_regs:
-                result.append(Register(name=reg, value=thread_state[reg]['value']))
-        else:
-            in_frames = False
-            for line in self._data.split('\n'):
-                if in_frames:
-                    splitted = line.split()
-
-                    if len(splitted) == 0:
-                        break
-
-                    for i in range(0, len(splitted), 2):
-                        register_name = splitted[i]
-                        if not register_name.endswith(':'):
-                            break
-
-                        register_name = register_name[:-1]
-                        register_value = int(splitted[i + 1], 16)
-
-                        result.append(Register(name=register_name, value=register_value))
-
-                if line.startswith(f'Thread {self.faulting_thread} crashed with ARM Thread State'):
-                    in_frames = True
-
-        return result
-
-    @cached_property
-    def exception_type(self):
-        if self._is_json:
-            return self._data['exception'].get('type')
-        else:
-            return self._parse_field('Exception Type')
-
-    @cached_property
-    def exception_subtype(self) -> Optional[str]:
-        if self._is_json:
-            return self._data['exception'].get('subtype')
-        else:
-            return self._parse_field('Exception Subtype')
-
-    @cached_property
-    def application_specific_information(self) -> str:
-        result = ''
-        if self._is_json:
-            return str(self._data.get('asi'))
-        else:
-            in_frames = False
-            for line in self._data.split('\n'):
-                if in_frames:
-                    line = line.strip()
-                    if len(line) == 0:
-                        break
-
-                    result += line + '\n'
-
-                if line.startswith('Application Specific Information:'):
-                    in_frames = True
-
-        return result
-
-    def __str__(self):
-        result = ''
-
-        result += click.style(f'{self.incident_id} {self.timestamp}\n\n', fg='cyan')
-
-        if self.bug_type not in ('109', '309', '327', '385'):
-            # these crashes aren't crash dumps
-            return result
-
-        result += click.style(f'Exception: {self.exception_type}\n', bold=True)
-
-        if self.exception_subtype:
-            result += click.style('Exception Subtype: ', bold=True)
-            result += f'{self.exception_subtype}\n'
-
-        if self.application_specific_information:
-            result += click.style('Application Specific Information: ', bold=True)
-            result += self.application_specific_information
-
-        result += '\n'
-
-        result += click.style('Registers:', bold=True)
-        for i, register in enumerate(self.registers):
-            if i % 4 == 0:
-                result += '\n'
-
-            result += f'{register.name} = 0x{register.value:016x} '.rjust(30)
-
-        result += '\n\n'
-
-        result += click.style('Frames:\n', bold=True)
-        for frame in self.frames:
-            result += f'\t[{frame.image}] {frame.symbol} + 0x{frame.offset:x}\n'
-
-        return result
-
-
-class CrashReports:
+class CrashReportsManager:
     COPY_MOBILE_NAME = 'com.apple.crashreportcopymobile'
     CRASH_MOVER_NAME = 'com.apple.crashreportmover'
 
@@ -265,7 +79,7 @@ class CrashReports:
                 continue
 
             crash_report_raw = self.afc.get_file_contents(filename).decode()
-            crash_report = CrashReport(crash_report_raw)
+            crash_report = CrashReport(crash_report_raw, filename=filename)
 
             if name is None or crash_report.name == name:
                 if raw:
@@ -300,3 +114,23 @@ class CrashReports:
 
         self.afc.wait_exists(sysdiagnose_filename)
         self.pull(out, entry=sysdiagnose_filename, erase=erase)
+
+
+parse_parser = Cmd2ArgumentParser(description='parse given crash report file')
+parse_parser.add_argument('filename')
+
+clear_parser = Cmd2ArgumentParser(description='remove all crash reports')
+
+
+class CrashReportsShell(AfcShell):
+    def __init__(self, lockdown: LockdownClient):
+        super().__init__(lockdown, service_name=CrashReportsManager.COPY_MOBILE_NAME)
+        self.manager = CrashReportsManager(lockdown)
+
+    @with_argparser(parse_parser)
+    def do_parse(self, args):
+        self.poutput(CrashReport(self.afc.get_file_contents(args.filename).decode(), filename=args.filename))
+
+    @with_argparser(clear_parser)
+    def do_clear(self, args):
+        self.manager.clear()
