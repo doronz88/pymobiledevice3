e2acacf224d6826fbd25aaa21fdaa342fdc393dc
netanelc305 <netanelc305@protonmail.com>
2023-03-29 07:50:13 -0700

    preboard: Add type hints

--- pymobiledevice3/services/preboard.py
+++ pymobiledevice3/services/preboard.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+
+from typing import Mapping
+
 from pymobiledevice3.lockdown import LockdownClient
 from pymobiledevice3.services.base_service import BaseService
 
@@ -7,11 +10,11 @@ from pymobiledevice3.services.base_service import BaseService
 class PreboardService(BaseService):
     SERVICE_NAME = 'com.apple.preboardservice_v2'
 
-    def __init__(self, lockdown: LockdownClient):
+    def __init__(self, lockdown: LockdownClient) -> None:
         super().__init__(lockdown, self.SERVICE_NAME)
 
-    def create_stashbag(self, manifest):
+    def create_stashbag(self, manifest: Mapping) -> Mapping:
         return self.service.send_recv_plist({'Command': 'CreateStashbag', 'Manifest': manifest})
 
-    def commit(self, manifest):
+    def commit(self, manifest: Mapping) -> Mapping:
         return self.service.send_recv_plist({'Command': 'CommitStashbag', 'Manifest': manifest})
