11173eb1476e50c7332f6cefce8499ab1ba7c31d
netanelc305 <netanelc305@protonmail.com>
2023-03-29 07:51:07 -0700

    power_assertion: Add type hints

--- pymobiledevice3/services/power_assertion.py
+++ pymobiledevice3/services/power_assertion.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+
+
 import contextlib
+from typing import Generator
 
 from pymobiledevice3.lockdown import LockdownClient
 from pymobiledevice3.services.base_service import BaseService
@@ -8,11 +11,12 @@ from pymobiledevice3.services.base_service import BaseService
 class PowerAssertionService(BaseService):
     SERVICE_NAME = 'com.apple.mobile.assertion_agent'
 
-    def __init__(self, lockdown: LockdownClient):
+    def __init__(self, lockdown: LockdownClient) -> None:
         super().__init__(lockdown, self.SERVICE_NAME)
 
     @contextlib.contextmanager
-    def create_power_assertion(self, type_: str, name: str, timeout: int, details: str = None):
+    def create_power_assertion(self, type_: str, name: str, timeout: int, details: str = None) -> Generator[
+        None, None, None]:
         msg = {
             'CommandKey': 'CommandCreateAssertion',
             'AssertionTypeKey': type_,
