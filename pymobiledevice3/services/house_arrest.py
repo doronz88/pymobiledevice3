ee95c6f00ccbb3f67e4fbe22e908714fe446df6d
netanelc305 <netanelc305@protonmail.com>
2023-03-29 21:37:46 -0700

    house_arrest: Add type hints

--- pymobiledevice3/services/house_arrest.py
+++ pymobiledevice3/services/house_arrest.py
@@ -7,13 +7,13 @@ from pymobiledevice3.services.afc import AfcService, AfcShell
 class HouseArrestService(AfcService):
     SERVICE_NAME = 'com.apple.mobile.house_arrest'
 
-    def __init__(self, lockdown: LockdownClient):
+    def __init__(self, lockdown: LockdownClient) -> None:
         self.logger = logging.getLogger(__name__)
         self.lockdown = lockdown
         service_name = self.SERVICE_NAME
         super(HouseArrestService, self).__init__(self.lockdown, service_name)
 
-    def send_command(self, bundle_id, cmd='VendContainer'):
+    def send_command(self, bundle_id: str, cmd: str = 'VendContainer') -> bool:
         self.service.send_plist({'Command': cmd, 'Identifier': bundle_id})
         res = self.service.recv_plist()
         if res.get('Error'):
@@ -22,7 +22,7 @@ class HouseArrestService(AfcService):
         else:
             return True
 
-    def shell(self, application_id, cmd='VendContainer'):
+    def shell(self, application_id: str, cmd: str = 'VendContainer') -> None:
         res = self.send_command(application_id, cmd)
         if res:
             AfcShell(self.lockdown, afc_service=self).cmdloop()
