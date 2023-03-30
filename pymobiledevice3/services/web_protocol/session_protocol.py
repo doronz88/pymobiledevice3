6205e08ade303596235a4f5e5423a844770a3fee
netanelc305 <netanelc305@protonmail.com>
2023-03-30 03:21:05 -0700

    session_protocol: Add type hints

--- pymobiledevice3/services/web_protocol/session_protocol.py
+++ pymobiledevice3/services/web_protocol/session_protocol.py
@@ -1,11 +1,12 @@
 import asyncio
 from functools import partial
+from typing import Any, Mapping
 
 from pymobiledevice3.exceptions import WirError
 
 
 class SessionProtocol:
-    def __init__(self, inspector, id_, app, page, method_prefix='Automation'):
+    def __init__(self, inspector, id_: str, app: str, page: int, method_prefix: str = 'Automation') -> None:
         """
         :param pymobiledevice3.services.webinspector.WebinspectorService inspector:
         """
@@ -16,7 +17,7 @@ class SessionProtocol:
         self.method_prefix = method_prefix
         self._wir_messages_id = 1
 
-    async def send_command(self, method, **kwargs):
+    async def send_command(self, method: str, **kwargs: Any) -> int:
         wir_id = self._wir_messages_id
         self._wir_messages_id += 1
         await self.inspector.send_socket_data(self.id_, self.app.id_, self.page.id_, {
@@ -26,26 +27,26 @@ class SessionProtocol:
         })
         return wir_id
 
-    async def get_response(self, wir_id):
+    async def get_response(self, wir_id: int) -> Any:
         response = await self.wait_for_message(wir_id)
         if 'result' in response:
             return response['result']
         elif 'error' in response:
             raise WirError(response['error']['message'])
 
-    async def send_receive(self, method, wait_for_response=True, **kwargs):
+    async def send_receive(self, method: str, wait_for_response: bool = True, **kwargs: Any) -> int:
         wir_id = await self.send_command(method, **kwargs)
         if wait_for_response:
             return await self.get_response(wir_id)
         else:
             return wir_id
 
-    async def wait_for_message(self, id_):
+    async def wait_for_message(self, id_: str) -> Mapping:
         while id_ not in self.inspector.wir_message_results:
             await asyncio.sleep(0)
         return self.inspector.wir_message_results.pop(id_)
 
-    def sync_send_receive(self, method, wait_for_response=True, **kwargs):
+    def sync_send_receive(self, method: str, wait_for_response: bool = True, **kwargs: Any) -> Any:
         return self.inspector.await_(self.send_receive(method, wait_for_response, **kwargs))
 
     def __getattr__(self, item):
