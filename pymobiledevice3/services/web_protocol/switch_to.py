413bf556da11de7e482aa83149f19d4a5421ab83
netanelc305 <netanelc305@protonmail.com>
2023-03-30 03:13:26 -0700

    switch_to: Add type hints

--- pymobiledevice3/services/web_protocol/switch_to.py
+++ pymobiledevice3/services/web_protocol/switch_to.py
@@ -1,10 +1,12 @@
+from typing import Union
+
 from pymobiledevice3.services.web_protocol.alert import Alert
-from pymobiledevice3.services.web_protocol.automation_session import By
+from pymobiledevice3.services.web_protocol.automation_session import AutomationSession, By
 from pymobiledevice3.services.web_protocol.element import WebElement
 
 
 class SwitchTo:
-    def __init__(self, session):
+    def __init__(self, session: AutomationSession) -> None:
         """
         :param pymobiledevice3.services.web_protocol.automation_session.AutomationSession session: Automation session.
         """
@@ -22,15 +24,14 @@ class SwitchTo:
         """ Switches focus to an alert on the page. """
         return Alert(self.session)
 
-    def default_content(self):
+    def default_content(self) -> None:
         """ Switch focus to the default frame. """
         self.session.switch_to_browsing_context('')
 
-    def frame(self, frame_reference):
+    def frame(self, frame_reference: Union[int, WebElement, str]) -> None:
         """
         Switches focus to the specified frame, by index, name, or web element.
-        :param frame_reference: The name of the window to switch to, an integer representing the index,
-                                or a web element that is an (i)frame to switch to.
+        :param frame_reference: The name of the window to switch to, an integer representing the index,or a web element that is an (i)frame to switch to.
         """
         if isinstance(frame_reference, int) or isinstance(frame_reference, WebElement):
             frame = frame_reference
@@ -48,11 +49,11 @@ class SwitchTo:
         else:
             self.session.switch_to_frame(frame_handle=frame)
 
-    def new_window(self, type_=''):
+    def new_window(self, type_: str = '') -> None:
         """ Switches to a new top-level browsing context. """
         self.session.switch_to_window(self.session.create_window(type_))
 
-    def parent_frame(self):
+    def parent_frame(self) -> None:
         """
         Switches focus to the parent context. If the current context is the top
         level browsing context, the context remains unchanged.
@@ -62,6 +63,6 @@ class SwitchTo:
                                                       self.session.current_parent_handle)
         self.session.switch_to_browsing_context(self.session.current_parent_handle)
 
-    def window(self, window_name):
+    def window(self, window_name: str) -> None:
         """ Switches focus to the specified window. """
         self.session.switch_to_window(window_name)
