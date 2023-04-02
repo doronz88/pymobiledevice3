e0d83f910416fcab6c4045af3469658627f188b5
netanelc305 <netanelc305@protonmail.com>
2023-04-01 23:04:13 -0700

    dvt_secure_socket_proxy: Add type hints

--- pymobiledevice3/services/dvt/dvt_secure_socket_proxy.py
+++ pymobiledevice3/services/dvt/dvt_secure_socket_proxy.py
@@ -8,7 +8,7 @@ class DvtSecureSocketProxyService(RemoteServer):
     SERVICE_NAME = 'com.apple.instruments.remoteserver.DVTSecureSocketProxy'
     OLD_SERVICE_NAME = 'com.apple.instruments.remoteserver'
 
-    def __init__(self, lockdown: LockdownClient):
+    def __init__(self, lockdown: LockdownClient) -> None:
         if Version(lockdown.product_version) >= Version('14.0'):
             service_name = self.SERVICE_NAME
             remove_ssl_context = False
