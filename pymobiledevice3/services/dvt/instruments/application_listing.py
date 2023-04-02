9b52b3664c406dd34bed296880d2bbb1587a345f
netanelc305 <netanelc305@protonmail.com>
2023-04-01 23:47:28 -0700

    application_listing: Add type hints

--- pymobiledevice3/services/dvt/instruments/application_listing.py
+++ pymobiledevice3/services/dvt/instruments/application_listing.py
@@ -1,10 +1,11 @@
+from pymobiledevice3.services.dvt.dvt_secure_socket_proxy import DvtSecureSocketProxyService
 from pymobiledevice3.services.remote_server import MessageAux
 
 
 class ApplicationListing:
     IDENTIFIER = 'com.apple.instruments.server.services.device.applictionListing'
 
-    def __init__(self, dvt):
+    def __init__(self, dvt: DvtSecureSocketProxyService) -> None:
         self._channel = dvt.make_channel(self.IDENTIFIER)
 
     def applist(self) -> list:
