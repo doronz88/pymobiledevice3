522af3020568b449d3c7b04cea1024ef0475296f
doron zarhi <doron88@gmail.com>
2023-04-23 10:18:56 +0300

    InstallationProxyService: refactor

--- pymobiledevice3/services/installation_proxy.py
+++ pymobiledevice3/services/installation_proxy.py
@@ -1,6 +1,8 @@
 import os
 import posixpath
+from typing import Callable, List, Mapping
 
+from pymobiledevice3.exceptions import AppInstallError
 from pymobiledevice3.lockdown import LockdownClient
 from pymobiledevice3.services.afc import AfcService
 from pymobiledevice3.services.base_service import BaseService
@@ -19,58 +21,68 @@ class InstallationProxyService(BaseService):
     def __init__(self, lockdown: LockdownClient):
         super().__init__(lockdown, self.SERVICE_NAME)
 
-    def watch_completion(self, handler=None, *args):
+    def _watch_completion(self, handler: Callable = None, *args) -> None:
         while True:
             response = self.service.recv_plist()
             if not response:
                 break
             error = response.get('Error')
             if error:
-                raise IOError(f'''{error}: {response.get('ErrorDescription')}''')
+                raise AppInstallError(f'{error}: {response.get("ErrorDescription")}')
             completion = response.get('PercentComplete')
             if completion:
                 if handler:
                     self.logger.debug('calling handler')
                     handler(completion, *args)
-                self.logger.info('%s %% Complete', response.get('PercentComplete'))
+                self.logger.info(f'{response.get("PercentComplete")}% Complete')
             if response.get('Status') == 'Complete':
-                return response.get('Status')
-        return 'Error'
+                return
+        raise AppInstallError()
 
-    def send_cmd_for_bid(self, bid, cmd='Archive', options=None, handler=None, *args):
+    def send_cmd_for_bundle_identifier(self, bundle_identifier: str, cmd: str = 'Archive', options: Mapping = None,
+                                       handler: Mapping = None, *args) -> None:
+        """ send a low-level command to installation relay """
         cmd = {'Command': cmd,
-               'ApplicationIdentifier': bid}
-        if options:
-            cmd.update({'ClientOptions': options})
+               'ApplicationIdentifier': bundle_identifier}
+
+        if options is None:
+            options = {}
+
+        cmd.update({'ClientOptions': options})
         self.service.send_plist(cmd)
-        self.logger.info('%s : %s\n', cmd, self.watch_completion(handler, *args))
+        self._watch_completion(handler, *args)
 
-    def uninstall(self, bid, options=None, handler=None, *args):
-        return self.send_cmd_for_bid(bid, 'Uninstall', options, handler, args)
+    def install(self, ipa_path: str, options: Mapping = None, handler: Callable = None, *args) -> None:
+        """ install given ipa from device path """
+        self.install_from_local(ipa_path, 'Install', options, handler, args)
 
-    def install_from_local(self, ipa_path, cmd='Install', options=None, handler=None, *args):
+    def upgrade(self, ipa_path: str, options: Mapping = None, handler: Callable = None, *args) -> None:
+        """ upgrade given ipa from device path """
+        self.install_from_local(ipa_path, 'Upgrade', options, handler, args)
+
+    def restore(self, bundle_identifier: str, options: Mapping = None, handler: Callable = None, *args) -> None:
+        """ no longer supported on newer iOS versions """
+        self.send_cmd_for_bundle_identifier(bundle_identifier, 'Restore', options, handler, args)
+
+    def uninstall(self, bundle_identifier: str, options: Mapping = None, handler: Callable = None, *args) -> None:
+        """ uninstall given bundle_identifier """
+        self.send_cmd_for_bundle_identifier(bundle_identifier, 'Uninstall', options, handler, args)
+
+    def install_from_local(self, ipa_path: str, cmd='Install', options: Mapping = None, handler: Callable = None,
+                           *args) -> None:
+        """ upload given ipa onto device and install it """
         if options is None:
             options = {}
         remote_path = posixpath.join('/', os.path.basename(ipa_path))
-        afc = AfcService(self.lockdown)
-        afc.set_file_contents(remote_path, open(ipa_path, 'rb').read())
+        with AfcService(self.lockdown) as afc:
+            afc.set_file_contents(remote_path, open(ipa_path, 'rb').read())
         cmd = {'Command': cmd,
                'ClientOptions': options,
                'PackagePath': remote_path}
         self.service.send_plist(cmd)
-        self.watch_completion(handler, args)
+        self._watch_completion(handler, args)
 
-    def install(self, ipa_path, options=None, handler=None, *args):
-        if options is None:
-            options = {}
-        return self.install_from_local(ipa_path, 'Install', options, handler, args)
-
-    def upgrade(self, ipa_path, options=None, handler=None, *args):
-        if options is None:
-            options = {}
-        return self.install_from_local(ipa_path, 'Upgrade', options, handler, args)
-
-    def check_capabilities_match(self, capabilities, options=None):
+    def check_capabilities_match(self, capabilities: Mapping = None, options: Mapping = None) -> Mapping:
         if options is None:
             options = {}
         cmd = {'Command': 'CheckCapabilitiesMatch',
@@ -79,94 +91,47 @@ class InstallationProxyService(BaseService):
         if capabilities:
             cmd['Capabilities'] = capabilities
 
-        self.service.send_plist(cmd)
-        result = self.service.recv_plist().get('LookupResult')
-        return result
+        return self.service.send_recv_plist(cmd).get('LookupResult')
 
-    def browse(self, options=None, attributes=None):
+    def browse(self, bundle_identifier: str, options: Mapping = None, attributes: List[str] = None) -> List[Mapping]:
         if options is None:
             options = {}
         if attributes:
             options['ReturnAttributes'] = attributes
 
         cmd = {'Command': 'Browse',
-               'ClientOptions': options}
+               'ClientOptions': options,
+               'ApplicationIdentifier': bundle_identifier}
 
         self.service.send_plist(cmd)
 
         result = []
         while True:
-            z = self.service.recv_plist()
-            if not z:
+            response = self.service.recv_plist()
+            if not response:
                 break
 
-            data = z.get('CurrentList')
-            if data:
+            data = response.get('CurrentList')
+            if data is not None:
                 result += data
 
-            if z.get('Status') == 'Complete':
+            if response.get('Status') == 'Complete':
                 break
 
         return result
 
-    def apps_info(self, options=None):
+    def lookup(self, options: Mapping = None) -> Mapping:
+        """ search installation database """
         if options is None:
             options = {}
-        cmd = {'Command': 'Lookup',
-               'ClientOptions': options}
-
-        self.service.send_plist(cmd)
-        return self.service.recv_plist().get('LookupResult')
-
-    def archive(self, bid, options=None, handler=None, *args):
-        if options is None:
-            options = {}
-        self.send_cmd_for_bid(bid, 'Archive', options, handler, args)
-
-    def restore_archive(self, bid, options=None, handler=None, *args):
-        if options is None:
-            options = {}
-        self.send_cmd_for_bid(bid, 'Restore', options, handler, args)
-
-    def remove_archive(self, bid, options=None, handler=None, *args):
-        if options is None:
-            options = {}
-        self.send_cmd_for_bid(bid, 'RemoveArchive', options, handler, args)
-
-    def archives_info(self, options=None):
-        if options is None:
-            options = {}
-        cmd = {'Command': 'LookupArchive',
-               'ClientOptions': options}
+        cmd = {'Command': 'Lookup', 'ClientOptions': options}
         return self.service.send_recv_plist(cmd).get('LookupResult')
 
-    def search_path_for_bid(self, bid):
-        path = None
-        for a in self.get_apps(app_types=['User', 'System']):
-            if a.get('CFBundleIdentifier') == bid:
-                path = a.get('Path') + '/' + a.get('CFBundleExecutable')
-        return path
-
-    def get_apps(self, app_types=None):
-        if app_types is None:
-            app_types = ['User']
-        return [app for app in self.apps_info().values()
-                if app.get('ApplicationType') in app_types]
-
-    def print_apps(self, app_types=None):
-        if app_types is None:
-            app_types = ['User']
-        for app in self.get_apps(app_types):
-            print(('%s : %s => %s' % (app.get('CFBundleDisplayName'),
-                                      app.get('CFBundleIdentifier'),
-                                      app.get('Path') if app.get('Path')
-                                      else app.get('Container'))).encode('utf-8'))
-
-    def get_apps_bid(self, app_types=None):
-        if app_types is None:
-            app_types = ['User']
-        return [app['CFBundleIdentifier']
-                for app in self.get_apps(app_types)]
-
-    def close(self):
-        self.service.close()
+    def get_apps(self, app_types: List[str] = None) -> List[Mapping]:
+        """ get applications according to given criteria """
+        lookup_result = self.lookup().values()
+        result = []
+        for app in lookup_result:
+            if (app_types is None) or (app['ApplicationType'] in app_types):
+                result.append(app)
+        return result
