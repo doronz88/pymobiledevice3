57854ec7844471fd0166f8dba8c930c2a874c1f6
Guy Salton <guy123121@gmail.com>
2023-06-02 03:11:40 +0300

    test_afc_shell: make TestPull class and add tests

--- tests/services/test_afc_shell.py
+++ tests/services/test_afc_shell.py
@@ -1,3 +1,5 @@
+import os
+import pathlib
 from pathlib import Path
 from unittest import mock
 
@@ -121,19 +123,6 @@ def test_push_completion(afc_shell, tmp_path: Path):
     assert 'Books' not in second_completion
 
 
-def test_pull_completion(afc_shell, tmp_path: Path):
-    completions = get_completions('pull D', 'D', afc_shell)
-    assert 'DCIM' in completions
-    assert 'Downloads' in completions
-    assert 'Books' not in completions
-
-    (tmp_path / 'temp1.txt').write_text('hey1')
-    (tmp_path / 'temp2.txt').write_text('hey2')
-    assert get_completions(f'pull DCIM {tmp_path}', str(tmp_path), afc_shell) == [f'{tmp_path} ']
-    assert get_completions(f'pull DCIM {tmp_path}/', f'{tmp_path}/', afc_shell) == [f'{tmp_path}/temp1.txt',
-                                                                                    f'{tmp_path}/temp2.txt']
-
-
 def test_ls(afc_shell):
     out = afc_shell.app_cmd('ls')
     assert isinstance(out, CommandResult)
@@ -143,216 +132,292 @@ def test_ls(afc_shell):
     assert 'Books' in filenames
 
 
-def test_pull_after_cd_single_file(afc_shell, tmp_path: Path):
-    """
-    source:
-    temp1
-    └── temp.txt
+class TestPull:
+    FILE_NAME = 'temp.txt'
+    FILE_NAME1 = 'temp1.txt'
+    FILE_DATA = b'data'
+    FILE_DATA1 = b'data1'
+    TEMP1 = 'temp1'
+    TEMP2 = 'temp2'
+    TEMP3 = 'temp3'
+    TEMP4 = 'temp4'
 
-    cd temp1
-    pull temp.txt target
+    @pytest.fixture()
+    def make_temp_dirs_and_files(self, afc_shell):
+        """
+        source:
+        temp1
+        └── temp2
+            └── temp3
+                ├── temp.txt
+                └── temp4
+                    └── temp1.txt
+        """
+        full_path = f'{self.TEMP1}/{self.TEMP2}/{self.TEMP3}/{self.TEMP4}'
+        afc_shell.afc.makedirs(full_path)
+        afc_shell.afc.set_file_contents(f'{self.TEMP1}/{self.TEMP2}/{self.TEMP3}/{self.FILE_NAME}', self.FILE_DATA)
+        afc_shell.afc.set_file_contents(f'{full_path}/{self.FILE_NAME1}', self.FILE_DATA1)
+        yield
+        afc_shell.afc.rm(self.TEMP1)
 
-    target
-    └── temp.txt
-    """
-    file_name = 'temp.txt'
-    file_data = b'data'
-    afc_shell.afc.makedirs('temp1')
-    afc_shell.afc.set_file_contents(f'temp1/{file_name}', file_data)
-    out = afc_shell.app_cmd(f'pull {file_name} {tmp_path.absolute()}')
-    try:
+    def test_pull_completion(self, afc_shell, tmp_path: Path):
+        completions = get_completions('pull D', 'D', afc_shell)
+        assert 'DCIM' in completions
+        assert 'Downloads' in completions
+        assert 'Books' not in completions
+
+        (tmp_path / 'temp1.txt').write_text('hey1')
+        (tmp_path / 'temp2.txt').write_text('hey2')
+        assert get_completions(f'pull DCIM {tmp_path}', str(tmp_path), afc_shell) == [f'{tmp_path} ']
+        assert get_completions(f'pull DCIM {tmp_path}/', f'{tmp_path}/', afc_shell) == [f'{tmp_path}/temp1.txt',
+                                                                                        f'{tmp_path}/temp2.txt']
+
+    def test_pull_afc_file_not_found_error(self, afc_shell, tmp_path: Path):
+        """
+        Raises: AfcFileNotFoundError
+        """
+        out = afc_shell.app_cmd(f'pull {self.FILE_NAME} {tmp_path.absolute()}')
         assert 'AfcFileNotFoundError' in out.stderr
-        afc_shell.app_cmd('cd temp1')
-        out = afc_shell.app_cmd(f'pull {file_name} {tmp_path.absolute()}')
+
+    def test_pull_file_to_non_exists_folder(self, afc_shell, tmp_path: Path, make_temp_dirs_and_files):
+        """
+        Raises: NotADirectoryError
+        """
+        dst = f'{tmp_path}/temp1/'
+        out = afc_shell.app_cmd(
+            f'pull {self.TEMP1}/{self.TEMP2}/{self.TEMP3}/{self.FILE_NAME} {dst}')
+        assert 'NotADirectoryError' in out.stderr
+        assert f'pull: directory {pathlib.PurePosixPath(dst)} does not exist' in out.stderr
+
+    def test_pull_file_to_folder_with_slash(self, afc_shell, tmp_path: Path, make_temp_dirs_and_files):
+        """
+        cd temp3
+        pull temp.txt target/
+
+        target
+        └── temp.txt
+        """
+        afc_shell.app_cmd(f'cd {self.TEMP1}/{self.TEMP2}/{self.TEMP3}')
+        out = afc_shell.app_cmd(f'pull {self.FILE_NAME} {tmp_path.absolute()}/')
         assert not out.stderr
         assert len(list(tmp_path.iterdir())) == 1
-        assert (tmp_path / file_name).read_bytes() == file_data
-    finally:
-        afc_shell.afc.rm('temp1')
+        assert (tmp_path / self.FILE_NAME).read_bytes() == self.FILE_DATA
 
+    def test_pull_file_with_prefix_to_folder(self, afc_shell, tmp_path: Path, make_temp_dirs_and_files):
+        """
+        cd temp1
+        pull temp2/temp3/temp.txt target
 
-def test_pull_after_cd_single_file_with_prefix(afc_shell, tmp_path: Path):
-    """
-    source:
-    temp1
-    └── temp2
-        └── temp3
-            └── temp.txt
-
-    cd temp1
-    pull temp2/temp3/temp.txt target
-
-    target
-    └── temp.txt
-    """
-    file_name = 'temp.txt'
-    file_data = b'data'
-    afc_shell.afc.makedirs('temp1/temp2/temp3')
-    afc_shell.afc.set_file_contents(f'temp1/temp2/temp3/{file_name}', file_data)
-    try:
-        afc_shell.app_cmd('cd temp1')
-        out = afc_shell.app_cmd(f'pull temp2/temp3/{file_name} {tmp_path.absolute()}')
+        target
+        └── temp.txt
+        """
+        afc_shell.app_cmd(f'cd {self.TEMP1}')
+        out = afc_shell.app_cmd(f'pull {self.TEMP2}/{self.TEMP3}/{self.FILE_NAME} {tmp_path.absolute()}')
         assert not out.stderr
         assert len(list(tmp_path.iterdir())) == 1
-        assert (tmp_path / file_name).read_bytes() == file_data
-    finally:
-        afc_shell.afc.rm('temp1')
+        assert (tmp_path / self.FILE_NAME).read_bytes() == self.FILE_DATA
 
+    def test_pull_file_to_current_folder(self, afc_shell, tmp_path: Path, make_temp_dirs_and_files):
+        """
+        cd temp3
+        pull temp.txt .
 
-def test_pull_after_cd_single_file_with_rename(afc_shell, tmp_path: Path):
-    """
-    source:
-    temp1
-    └── temp.txt
-
-    cd temp1
-    pull temp.txt target/temp1.txt
-
-    target
-    └── temp1.txt
-    """
-    file_name = 'temp.txt'
-    new_file_path = tmp_path / 'temp1.txt'
-    file_data = b'data'
-    afc_shell.afc.makedirs('temp1')
-    afc_shell.afc.set_file_contents(f'temp1/{file_name}', file_data)
-    try:
-        afc_shell.app_cmd('cd temp1')
-        out = afc_shell.app_cmd(f'pull {file_name} {new_file_path.absolute()}')
+        target
+        └── temp.txt
+        """
+        afc_shell.app_cmd(f'cd {self.TEMP1}/{self.TEMP2}/{self.TEMP3}')
+        os.chdir(tmp_path)
+        out = afc_shell.app_cmd(f'pull {self.FILE_NAME} .')
         assert not out.stderr
         assert len(list(tmp_path.iterdir())) == 1
-        assert new_file_path.read_bytes() == file_data
-    finally:
-        afc_shell.afc.rm('temp1')
+        file = pathlib.Path(self.FILE_NAME)
+        assert file.read_bytes() == self.FILE_DATA
 
+    def test_pull_file_to_new_file_name(self, afc_shell, tmp_path: Path, make_temp_dirs_and_files):
+        """
+        cd temp3
+        pull temp.txt temp1.txt
 
-def test_pull_after_cd_recursive(afc_shell, tmp_path: Path):
-    """
-    source:
-    temp1
-    └── temp2
+        target
+        └── temp1.txt
+        """
+        afc_shell.app_cmd(f'cd {self.TEMP1}/{self.TEMP2}/{self.TEMP3}')
+        os.chdir(tmp_path)
+        out = afc_shell.app_cmd(f'pull {self.FILE_NAME} {self.FILE_NAME1}')
+        assert not out.stderr
+        assert len(list(tmp_path.iterdir())) == 1
+        file = pathlib.Path(self.FILE_NAME1)
+        assert file.read_bytes() == self.FILE_DATA
+
+    def test_pull_file_to_new_file_name_in_folder(self, afc_shell, tmp_path: Path, make_temp_dirs_and_files):
+        """
+        cd temp3
+        pull temp.txt target/temp1.txt
+
+        target
+        └── temp1.txt
+        """
+        new_file_path = tmp_path / self.FILE_NAME1
+        afc_shell.app_cmd(f'cd {self.TEMP1}/{self.TEMP2}/{self.TEMP3}')
+        out = afc_shell.app_cmd(f'pull {self.FILE_NAME} {new_file_path.absolute()}')
+        assert not out.stderr
+        assert len(list(tmp_path.iterdir())) == 1
+        assert new_file_path.read_bytes() == self.FILE_DATA
+
+    def test_pull_file_content_to_existing_file(self, afc_shell, tmp_path: Path, make_temp_dirs_and_files):
+        """
+        cd temp3
+        pull temp.txt target/temp1.txt
+
+        target
+        └── temp1.txt
+        """
+        exist_file = tmp_path / self.FILE_NAME1
+        exist_file.touch()
+        afc_shell.app_cmd(f'cd {self.TEMP1}/{self.TEMP2}/{self.TEMP3}')
+        out = afc_shell.app_cmd(f'pull {self.FILE_NAME} {exist_file.absolute()}')
+        assert not out.stderr
+        assert len(list(tmp_path.iterdir())) == 1
+        assert exist_file.read_bytes() == self.FILE_DATA
+
+    def test_pull_folder_to_existing_file(self, afc_shell, tmp_path, make_temp_dirs_and_files):
+        """
+        pull temp1 target/temp.txt
+
+        Raises: IsADirectoryError
+        """
+        new_file_path = tmp_path / self.FILE_NAME
+        new_file_path.touch()
+        src = self.TEMP1
+        out = afc_shell.app_cmd(f'pull {src} {new_file_path.absolute()}')
+        assert 'IsADirectoryError' in out.stderr
+        assert f'pull: {afc_shell.curdir}{src} is a directory (not copied).' in out.stderr
+
+    def test_pull_folder_to_folder(self, afc_shell, tmp_path: Path, make_temp_dirs_and_files):
+        """
+        cd temp1/temp2
+        pull temp3 target
+
+        target
         └── temp3
             ├── temp.txt
             └── temp4
                 └── temp1.txt
-
-    cd temp1/temp2
-    pull temp3 target
-
-    target
-    └── temp3
-        ├── temp.txt
-        └── temp4
-            └── temp1.txt
-    """
-    file_name = 'temp.txt'
-    file_name1 = 'temp.txt'
-    file_data = b'data'
-    file_data1 = b'data1'
-    afc_shell.afc.makedirs('temp1/temp2/temp3/temp4')
-    afc_shell.afc.set_file_contents(f'temp1/temp2/temp3/{file_name}', file_data)
-    afc_shell.afc.set_file_contents(f'temp1/temp2/temp3/temp4/{file_name1}', file_data1)
-    try:
-        afc_shell.app_cmd('cd temp1/temp2')
-        out = afc_shell.app_cmd(f'pull temp3 {tmp_path.absolute()}')
+        """
+        afc_shell.app_cmd(f'cd {self.TEMP1}/{self.TEMP2}')
+        out = afc_shell.app_cmd(f'pull {self.TEMP3} {tmp_path.absolute()}')
         assert not out.stderr
         assert len(list(tmp_path.iterdir())) == 1
 
-        temp3 = tmp_path / 'temp3'
+        temp3 = tmp_path / self.TEMP3
         assert len(list(temp3.iterdir())) == 2
-        assert (temp3 / file_name).read_bytes() == file_data
+        assert (temp3 / self.FILE_NAME).read_bytes() == self.FILE_DATA
 
-        temp4 = temp3 / 'temp4'
+        temp4 = temp3 / self.TEMP4
         assert len(list(temp4.iterdir())) == 1
-        assert (temp4 / file_name1).read_bytes() == file_data1
-    finally:
-        afc_shell.afc.rm('temp1')
+        assert (temp4 / self.FILE_NAME1).read_bytes() == self.FILE_DATA1
 
+    def test_pull_current_folder_to_folder(self, afc_shell, tmp_path: Path, make_temp_dirs_and_files):
+        """
+        cd temp1/temp2/temp3
+        pull . target
 
-def test_pull_after_cd_recursive_current(afc_shell, tmp_path: Path):
-    """
-    source:
-    temp1
-    └── temp2
+        target
         └── temp3
             ├── temp.txt
             └── temp4
                 └── temp1.txt
-
-    cd temp1/temp2/temp3
-    pull . target
-
-    target
-    └── temp3
-        ├── temp.txt
-        └── temp4
-            └── temp1.txt
-    """
-    file_name = 'temp.txt'
-    file_name1 = 'temp1.txt'
-    file_data = b'data'
-    file_data1 = b'data1'
-    afc_shell.afc.makedirs('temp1/temp2/temp3/temp4')
-    afc_shell.afc.set_file_contents(f'temp1/temp2/temp3/{file_name}', file_data)
-    afc_shell.afc.set_file_contents(f'temp1/temp2/temp3/temp4/{file_name1}', file_data1)
-    try:
-        afc_shell.app_cmd('cd temp1/temp2/temp3')
+        """
+        afc_shell.app_cmd(f'cd {self.TEMP1}/{self.TEMP2}/{self.TEMP3}')
         out = afc_shell.app_cmd(f'pull . {tmp_path.absolute()}')
         assert not out.stderr
         assert len(list(tmp_path.iterdir())) == 1
 
-        temp3 = tmp_path / 'temp3'
+        temp3 = tmp_path / self.TEMP3
         assert len(list(temp3.iterdir())) == 2
-        assert (temp3 / file_name).read_bytes() == file_data
+        assert (temp3 / self.FILE_NAME).read_bytes() == self.FILE_DATA
 
-        temp4 = temp3 / 'temp4'
+        temp4 = temp3 / self.TEMP4
         assert len(list(temp4.iterdir())) == 1
-        assert (temp4 / file_name1).read_bytes() == file_data1
-    finally:
-        afc_shell.afc.rm('temp1')
+        assert (temp4 / self.FILE_NAME1).read_bytes() == self.FILE_DATA1
 
+    def test_pull_folder_with_prefix_to_folder(self, afc_shell, tmp_path: Path, make_temp_dirs_and_files):
+        """
+        cd temp1
+        pull temp2/temp3 target
 
-def test_pull_after_cd_recursive_with_prefix(afc_shell, tmp_path: Path):
-    """
-    source:
-    temp1
-    └── temp2
+        target
         └── temp3
             ├── temp.txt
             └── temp4
                 └── temp1.txt
-
-    cd temp1
-    pull temp2/temp3 target
-
-    target
-    └── temp3
-        ├── temp.txt
-        └── temp4
-            └── temp1.txt
-    """
-    file_name = 'temp.txt'
-    file_name1 = 'temp.txt'
-    file_data = b'data'
-    file_data1 = b'data1'
-    afc_shell.afc.makedirs('temp1/temp2/temp3/temp4')
-    afc_shell.afc.set_file_contents(f'temp1/temp2/temp3/{file_name}', file_data)
-    afc_shell.afc.set_file_contents(f'temp1/temp2/temp3/temp4/{file_name1}', file_data1)
-    try:
-        afc_shell.app_cmd('cd temp1')
-        out = afc_shell.app_cmd(f'pull temp2/temp3 {tmp_path.absolute()}')
+        """
+        afc_shell.app_cmd(f'cd {self.TEMP1}')
+        out = afc_shell.app_cmd(f'pull {self.TEMP2}/{self.TEMP3} {tmp_path.absolute()}')
         assert not out.stderr
         assert len(list(tmp_path.iterdir())) == 1
 
-        temp3 = tmp_path / 'temp3'
+        temp3 = tmp_path / self.TEMP3
         assert len(list(temp3.iterdir())) == 2
-        assert (temp3 / file_name).read_bytes() == file_data
+        assert (temp3 / self.FILE_NAME).read_bytes() == self.FILE_DATA
 
-        temp4 = temp3 / 'temp4'
+        temp4 = temp3 / self.TEMP4
         assert len(list(temp4.iterdir())) == 1
-        assert (temp4 / file_name1).read_bytes() == file_data1
-    finally:
-        afc_shell.afc.rm('temp1')
+        assert (temp4 / self.FILE_NAME1).read_bytes() == self.FILE_DATA1
+
+    def test_pull_folder_with_slash_to_non_exists_folder(self, afc_shell, tmp_path: Path, make_temp_dirs_and_files):
+        """
+        cd temp1/temp2
+        pull temp3/ target
+
+        target
+        └── temp1
+            ├── temp.txt
+            └── temp4
+                └── temp1.txt
+        """
+        afc_shell.app_cmd(f'cd {self.TEMP1}/{self.TEMP2}')
+        os.chdir(tmp_path)
+        out = afc_shell.app_cmd(f'pull {self.TEMP3}/ {self.TEMP1}')
+        assert not out.stderr
+        assert len(list(tmp_path.iterdir())) == 1
+
+        temp1 = tmp_path / self.TEMP1
+        assert len(list(temp1.iterdir())) == 2
+        assert (temp1 / self.FILE_NAME).read_bytes() == self.FILE_DATA
+
+        temp4 = temp1 / self.TEMP4
+        assert len(list(temp4.iterdir())) == 1
+        assert (temp4 / self.FILE_NAME1).read_bytes() == self.FILE_DATA1
+
+    def test_pull_folder_with_slash_to_folder(self, afc_shell, tmp_path: Path, make_temp_dirs_and_files):
+        """
+        cd temp1/temp2
+        pull temp3/ target
+
+        target
+        ├── temp.txt
+        └── temp4
+            └── temp1.txt
+        """
+        afc_shell.app_cmd(f'cd {self.TEMP1}/{self.TEMP2}')
+        out = afc_shell.app_cmd(f'pull {self.TEMP3}/ {tmp_path.absolute()}')
+        assert not out.stderr
+        assert len(list(tmp_path.iterdir())) == 2
+        assert (tmp_path / self.FILE_NAME).read_bytes() == self.FILE_DATA
+
+        temp4 = tmp_path / self.TEMP4
+        assert len(list(temp4.iterdir())) == 1
+        assert (temp4 / self.FILE_NAME1).read_bytes() == self.FILE_DATA1
+
+    def test_pull_folder_to_not_exists_path(self, afc_shell, tmp_path, make_temp_dirs_and_files):
+        """
+        Raises: FileNotFoundError
+        """
+        dst = f'{tmp_path}/temp1/temp2'
+        out = afc_shell.app_cmd(f'pull {self.TEMP1} {dst}')
+        assert 'FileNotFoundError' in out.stderr
+        assert f'pull: {dst}: No such file or directory' in out.stderr
 
 
 def test_push_after_cd_single_file_current(afc_shell, tmp_path: Path):
