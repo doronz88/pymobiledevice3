89a1df6aa616b083e41383bb1e5e8c7ea79effc5
doronz <doron88@gmail.com>
2023-08-10 08:38:39 +0300

    core_device_tunnel_service: fully implement a working tunnel

--- pymobiledevice3/cli/remote.py
+++ pymobiledevice3/cli/remote.py
@@ -5,11 +5,16 @@ from typing import List, TextIO
 import click
 from cryptography.hazmat.primitives.asymmetric import rsa
 
-from pymobiledevice3.cli.cli_common import RSDCommand, print_json
+from pymobiledevice3.cli.cli_common import RSDCommand, print_json, prompt_device_list
 from pymobiledevice3.remote.bonjour import get_remoted_addresses
-from pymobiledevice3.remote.core_device_tunnel_service import create_core_device_tunnel_service
 from pymobiledevice3.remote.remote_service_discovery import RSD_PORT, RemoteServiceDiscoveryService
 from pymobiledevice3.remote.utils import stop_remoted
+
+try:
+    from pymobiledevice3.remote.core_device_tunnel_service import create_core_device_tunnel_service
+except ImportError:
+    # isn't supported on Windows
+    pass
 
 logger = logging.getLogger(__name__)
 
@@ -57,21 +62,30 @@ def rsd_info(service_provider: RemoteServiceDiscoveryService, color: bool):
     print_json(service_provider.peer_info, colored=color)
 
 
-@remote_cli.command('create-listener', cls=RSDCommand)
-@click.option('-p', '--protocol', type=click.Choice(['quic', 'udp']))
-@click.option('--color/--no-color', default=True)
-def create_listener(service_provider: RemoteServiceDiscoveryService, protocol: str, color: bool):
-    """ start a remote listener """
+async def start_quic_tunnel(service_provider: RemoteServiceDiscoveryService, secrets: TextIO) -> None:
     private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
     with create_core_device_tunnel_service(service_provider, autopair=True) as service:
-        print_json(service.create_listener(private_key, protocol=protocol), colored=color)
+        async with service.start_quic_tunnel(private_key, secrets_log_file=secrets) as tunnel_result:
+            if secrets is not None:
+                print(click.style('Secrets: ', bold=True, fg='magenta') +
+                      click.style(secrets.name, bold=True, fg='white'))
+            print(click.style('Interface: ', bold=True, fg='yellow') +
+                  click.style(tunnel_result.interface, bold=True, fg='white'))
+            print(click.style('RSD Address: ', bold=True, fg='yellow') +
+                  click.style(tunnel_result.address, bold=True, fg='white'))
+            print(click.style('RSD Port: ', bold=True, fg='yellow') +
+                  click.style(tunnel_result.port, bold=True, fg='white'))
+            print(click.style('Use the follow connection option:\n', bold=True, fg='yellow') +
+                  click.style(f'--rsd {tunnel_result.address} {tunnel_result.port}', bold=True, fg='cyan'))
+
+            while True:
+                # wait user input while the asyncio tasks execute
+                await asyncio.sleep(.5)
 
 
-@remote_cli.command('start-quic-tunnel', cls=RSDCommand)
-@click.option('--color/--no-color', default=True)
-def start_quic_tunnel(service_provider: RemoteServiceDiscoveryService, color: bool):
+@remote_cli.command('start-quic-tunnel')
+@click.option('--secrets', type=click.File('wt'), help='TLS keyfile for decrypting with Wireshark')
+def cli_start_quic_tunnel(secrets: TextIO):
     """ start quic tunnel """
-    logger.critical('This is a WIP command. Will only print the required parameters for the quic connection')
-    private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
-    with create_core_device_tunnel_service(service_provider, autopair=True) as service:
-        print_json(asyncio.run(service.start_quic_tunnel(private_key)), colored=color)
+    rsd = prompt_device_list(get_device_list())
+    asyncio.run(start_quic_tunnel(rsd, secrets), debug=True)
