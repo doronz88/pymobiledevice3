b46ce2072b2c168ec75625df10f6bf1e13c96175
doronz <doron88@gmail.com>
2023-11-29 10:53:57 +0200

    cli: fix permissions check on remote commands for windows

--- pymobiledevice3/cli/cli_common.py
+++ pymobiledevice3/cli/cli_common.py
@@ -3,6 +3,7 @@ import json
 import logging
 import os
 import signal
+import sys
 import uuid
 from typing import Callable, List, Mapping, Optional, Tuple
 
@@ -101,7 +102,7 @@ UDID_ENV_VAR = 'PYMOBILEDEVICE3_UDID'
 
 def sudo_required(func):
     def wrapper(*args, **kwargs):
-        if os.geteuid() != 0:
+        if sys.platform != 'win32' and os.geteuid() != 0:
             raise AccessDeniedError()
         else:
             func(*args, **kwargs)
