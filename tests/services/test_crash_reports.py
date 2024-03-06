954e7cf6317da858f09782fc893565df025e4d13
dario-br <77158872+dario-br@users.noreply.github.com>
2024-03-06 23:28:13 +0100

    crash_reports: add optional `timeout` to `get_new_sysdiagnose()`

--- tests/services/test_crash_reports.py
+++ tests/services/test_crash_reports.py
@@ -1,5 +1,6 @@
 import glob
 import shutil
+import time
 
 import pytest
 
@@ -74,7 +75,7 @@ def test_pull(crash_manager, delete_test_dir):
 
 
 @pytest.mark.parametrize(
-    ('message', 'expected'),
+    ('message', 'return_value'),
     (
         ('sysdiagnose (full) complete', True),
         ('Sysdiagnose completed. File path: /foo/bar.tar.gz', True),
@@ -82,5 +83,13 @@ def test_pull(crash_manager, delete_test_dir):
         ('', False),
     )
 )
-def test_sysdiagnose_syslog_message_match_return_value(message, expected):
-    assert CrashReportsManager._sysdiagnose_complete_syslog_match(message) is expected
+def test_sysdiagnose_syslog_message_match_return_value(message, return_value):
+    assert CrashReportsManager._sysdiagnose_complete_syslog_match(message) is return_value
+
+
+@pytest.mark.parametrize(
+    ('end_time', 'return_value'),
+    ((-1, True), (0, True), (time.monotonic()+10, False), (None, False))
+)
+def test_check_timeout(crash_manager, end_time, return_value):
+    assert crash_manager._check_timeout(end_time) is return_value
