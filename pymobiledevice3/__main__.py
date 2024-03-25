b9dc30468d6f496be5e1dc95ffcc71e0b81ad601
doronz88 <doron88@gmail.com>
2024-03-25 10:56:40 +0200

    cli: automatically add `--tunnel ''` on `InvalidServiceError`

--- pymobiledevice3/__main__.py
+++ pymobiledevice3/__main__.py
@@ -125,6 +125,9 @@ def main() -> None:
         logger.error('Developer Mode is disabled. You can try to enable it using: '
                      'python3 -m pymobiledevice3 amfi enable-developer-mode')
     except InvalidServiceError:
+        if '--tunnel' not in sys.argv:
+            sys.argv += ['--tunnel', '']
+            return main()
         logger.error(INVALID_SERVICE_MESSAGE)
     except NoDeviceSelectedError:
         return
