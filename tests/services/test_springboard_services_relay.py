ade4d66e12333416c8afc99ebbbbe894df1898a0
DoronZ <doron88@gmail.com>
2024-05-13 22:39:27 +0300

    tests: add springboard tests

--- tests/services/test_springboard_services_relay.py
+++ tests/services/test_springboard_services_relay.py
@@ -11,3 +11,29 @@ def test_get_icon_png_data(lockdown):
     with SpringBoardServicesService(lockdown) as springboard:
         icon_data = springboard.get_icon_pngdata("com.apple.weather")
         assert icon_data.startswith(PNG_HEADER)
+
+
+def test_get_icon_date(lockdown):
+    """
+    :param pymobiledevice3.lockdown.LockdownClient lockdown: Lockdown client.
+    """
+    with SpringBoardServicesService(lockdown) as springboard:
+        assert len(springboard.get_icon_state()[0]) > 0
+
+
+def test_set_icon_date(lockdown):
+    """
+    :param pymobiledevice3.lockdown.LockdownClient lockdown: Lockdown client.
+    """
+    with SpringBoardServicesService(lockdown) as springboard:
+        icon_state = springboard.get_icon_state()
+        # swap docker icons
+        icon_state[0] = icon_state[0][::-1]
+        springboard.set_icon_state(icon_state)
+
+        assert springboard.get_icon_state()[0] == icon_state[0]
+
+        icon_state[0] = icon_state[0][::-1]
+        springboard.set_icon_state(icon_state)
+
+        assert springboard.get_icon_state()[0] == icon_state[0]
