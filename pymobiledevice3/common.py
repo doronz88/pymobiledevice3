6b84af93d1b5bf77981127711448952d61125874
doronz88 <doron88@gmail.com>
2024-11-04 12:53:10 +0200

    common: make home folder creation use the right owner

--- pymobiledevice3/common.py
+++ pymobiledevice3/common.py
@@ -2,9 +2,11 @@ from pathlib import Path
 
 from pymobiledevice3.osu.os_utils import get_os_utils
 
-_HOMEFOLDER = get_os_utils().get_homedir() / '.pymobiledevice3'
+_OS_UTILS = get_os_utils()
+_HOMEFOLDER = _OS_UTILS.get_homedir() / '.pymobiledevice3'
 
 
 def get_home_folder() -> Path:
     _HOMEFOLDER.mkdir(exist_ok=True, parents=True)
+    _OS_UTILS.chown_to_non_sudo_if_needed(_HOMEFOLDER)
     return _HOMEFOLDER
