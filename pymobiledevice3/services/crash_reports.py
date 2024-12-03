8d33f4399c505c26f2af9b4b38f409ef7e444b36
Dario Borreguero Rincon <dario.borreguero-rincon@ec.europa.eu>
2024-12-03 11:39:25 +0100

    Added progress_bar option to AFC service when pulling files.

--- pymobiledevice3/services/crash_reports.py
+++ pymobiledevice3/services/crash_reports.py
@@ -79,13 +79,15 @@ class CrashReportsManager:
         """
         return list(self.afc.dirlist(path, depth))[1:]  # skip the root path '/'
 
-    def pull(self, out: str, entry: str = '/', erase: bool = False, match: Optional[str] = None) -> None:
+    def pull(self, out: str, entry: str = '/', erase: bool = False, match: Optional[str] = None,
+             progress_bar: bool = True) -> None:
         """
         Pull crash reports from the device.
         :param out: Directory to pull crash reports to.
         :param entry: File or Folder to pull.
         :param erase: Whether to erase the original file from the CrashReports directory.
         :param match: Regex to match against file and directory names to pull.
+        :param progress_bar: Whether to show a progress bar when pulling large files.
         """
 
         def log(src: str, dst: str) -> None:
@@ -95,7 +97,7 @@ class CrashReportsManager:
                     self.afc.rm_single(src, force=True)
 
         match = None if match is None else re.compile(match)
-        self.afc.pull(entry, out, match, callback=log)
+        self.afc.pull(entry, out, match, callback=log, progress_bar=progress_bar)
 
     def flush(self) -> None:
         """ Trigger com.apple.crashreportmover to flush all products into CrashReports directory """
