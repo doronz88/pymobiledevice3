6be57a74fafc480b85e68c77b3b03b087cfaf9b6
doronz88 <doron88@gmail.com>
2025-02-25 19:11:28 +0200

    service_connection: Retry on `IncompleteReadError`

--- pymobiledevice3/service_connection.py
+++ pymobiledevice3/service_connection.py
@@ -6,6 +6,7 @@ import ssl
 import struct
 import time
 import xml
+from asyncio import IncompleteReadError
 from enum import Enum
 from typing import Any, Optional
 
@@ -267,7 +268,13 @@ class ServiceConnection:
         :param size: The amount of data to receive.
         :return: The received data.
         """
-        return await self.reader.readexactly(size)
+        data = b''
+        while len(data) < size:
+            try:
+                data += await self.reader.readexactly(size)
+            except IncompleteReadError as e:
+                data += e.partial
+        return data
 
     async def aio_recv_prefixed(self, endianity: str = '>') -> bytes:
         """
